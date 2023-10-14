ffda68535d67203f0a0fe0d718439c25b0fac8e1
isaiah usiholo <evansaizik@gmail.com>
2023-10-14 11:54:15 +0100

    refreshToken test

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -16,30 +16,40 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
 
   if (result?.error?.status === 401) {
-    console.log('error is from point 1');
     localStorage.removeItem('loggedInUser');
-    // window.location.replace('/login');
+    window.location.replace('/login');
   }
 
   if (result?.error?.status === 403) {
     localStorage.removeItem('accessToken');
 
-    const refreshToken = await baseQuery('/users/refresh', api, {
-      method: 'GET',
-      credentials: 'include',
-    });
+    // Works on only desktop browsers
+    // const refreshToken = await baseQuery('/users/refresh', api, {
+    //   method: 'GET',
+    //   credentials: 'include',
+    // });
 
-    if (refreshToken?.data?.accessToken) {
-      localStorage.setItem('accessToken', refreshToken.data.accessToken);
+    // works on both mobile and desktop browsers
+    const fetchRefreshToken = await fetch(
+      'https://bubble-fg8r.onrender.com/api/v1/users/refresh',
+      {
+        credentials: 'include',
+      }
+    );
+
+    const refreshToken = await fetchRefreshToken.json();
+    console.log(refreshToken);
+
+    if (refreshToken?.accessToken) {
+      localStorage.setItem('accessToken', refreshToken.accessToken);
 
       result = await baseQuery(args, api, extraOptions);
     } else if (
       refreshToken?.error?.status === 403 ||
       refreshToken?.error?.status === 401
     ) {
-      console.log('error is from here point 2');
       localStorage.removeItem('loggedInUser');
-      // window.location.replace('/login')
+      window.location.replace('/login')
     }
   }
   return result;
